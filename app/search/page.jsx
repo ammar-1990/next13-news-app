c8aa5be27fa2532a7717c4b90e78aaa8d10c45e6
ammar <ammar.ali.haidar.1990@gmail.com>
2023-04-26 13:54:30 +0300

    complete

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -8,7 +8,7 @@ const page = async ({searchParams}) => {
 
   return (
     <div>
-<h1 className='font-serif text-3xl underline underline-offset-2 decoration-orange-400 decoration-2'>Search results for {searchParams.term}</h1>
+<h1 className='font-serif text-3xl underline underline-offset-2 p-4 decoration-orange-400 decoration-2'>Search results for {searchParams.term}</h1>
         <NewsList news={news} />
     </div>
   )
